b715ecc95e335b919076c1dd165dfa75934745d9
Bove <ribohe94@gmail.com>
2016-04-10 19:31:08 -0600

    Agrego botones

--- app/src/main/java/com/example/riccardo/juegomemoria/MainActivity.java
+++ app/src/main/java/com/example/riccardo/juegomemoria/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.riccardo.juegomemoria;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +13,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setToggleImg();
     }
+
+    //Cambio imagen de botón cuando se presiona
+
+    public void setToggleImg() {
+        final ToggleButton tglBtn = (ToggleButton) findViewById(R.id.imageButton);
+        tglBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    tglBtn.setBackgroundResource(R.drawable.button_play_pressed);
+                } else {
+                    tglBtn.setBackgroundResource(R.drawable.button_play_default);
+                }
+            }
+        });
+    }
+
+
 }
