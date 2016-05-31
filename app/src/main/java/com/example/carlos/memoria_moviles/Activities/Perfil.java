451788e20240337662a42e71e65e6f74ba9f2726
caracolferoz <carloshv93@gmail.com>
2016-05-31 00:46:17 -0600

    Perfil
    Actualizaci[on, ocupo la base de datos para no tener mis datos cargados
    por defecto

--- app/src/main/java/com/example/carlos/memoria_moviles/Activities/Perfil.java
+++ app/src/main/java/com/example/carlos/memoria_moviles/Activities/Perfil.java
@@ -1,15 +1,23 @@
 package com.example.carlos.memoria_moviles.Activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.carlos.memoria_moviles.R;
 
 public class Perfil extends AppCompatActivity {
-    ImageView picture;
+    private ImageView picture;
+    private TextView nombre;
+    private TextView correo;
+    private Usuario user;
+    Bitmap bmp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +32,28 @@ public class Perfil extends AppCompatActivity {
                 startActivityForResult(Intent.createChooser(intent, "Seleccione una foto de perfil"), 1);
             }
         });
-    }
-
-    public void onActivityResult(int reqCode,int resCode, Intent data){
-        if(resCode==RESULT_OK){
-            if(reqCode==1){
-                picture.setImageURI(data.getData());
-            }
-
+        nombre = (TextView) findViewById(R.id.txtNickName);
+        correo = (TextView) findViewById(R.id.txtEmail);
+        user = Usuario.getInstance();
+        user.setName("Carlos");
+        user.setEmail("carloshv93@gmail.com");
+        if (user.getName() != null) {
+            nombre.setText(user.getName());
+            correo.setText(user.getEmail());
+        } else {
+            user.setName(nombre.getText().toString());
+            user.setEmail(correo.getText().toString());
         }
-
     }
+
+    public void onActivityResult(int reqCode, int resCode, Intent data) {
+        super.onActivityResult(reqCode, resCode, data);
+        if (reqCode == 1) {
+            //Cambiar imagen
+            //He buscado pero no sirvem :S
+        }
+    }
+    public void Mensaje(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    };
 }
--- app/src/main/java/com/example/carlos/memoria_moviles/Activities/Usuario.java
+++ app/src/main/java/com/example/carlos/memoria_moviles/Activities/Usuario.java
@@ -1,30 +1,44 @@
 package com.example.carlos.memoria_moviles.Activities;
 
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
-
-public class Usuario extends AppCompatActivity {
-
-    ImageView picture;
+public class Usuario extends AppCompatActivity{
+    private Bitmap profilePicture;
+    private String name;
+    private String email;
 
     private static Usuario instance = null;
 
     protected Usuario() {
-
     }
     public static Usuario getInstance() {
         if(instance == null) {instance = new Usuario(); }
         return instance;
     }
 
-    public ImageView getPicture() {
-        return picture;
+    public Bitmap getProfilePicture() {
+        return profilePicture;
     }
 
-    public void setPicture(ImageView picture) {
-        this.picture = picture;
+    public void setProfilePicture(Bitmap profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }// fin de la clase de variables globales
-
